af58238d44f3c6065d81601feef8820f406aa092
Diane Ngani <diane.ngani@googlemail.com>
2020-04-30 19:20:51 +0200

    static final variables for every subclass
    some static final variables for a simpler comparison of classes. A help
    method for an instant log of the class variables in every subclass.

--- UnionFindAlgorithmen/src/ngani/algo/cc/unionfind/utils/UnionFindFrame.java
+++ UnionFindAlgorithmen/src/ngani/algo/cc/unionfind/utils/UnionFindFrame.java
@@ -9,11 +9,32 @@ package ngani.algo.cc.unionfind.utils;
  */
 public abstract class UnionFindFrame {
 	
+	/**
+	 * A static final value representing the subclass with the same name. 
+	 * Instead of using isInstanceof(), we just give this integer value as parameter.
+	 */
 	public static final int SIMPLE = 1;
+	/**
+	 * A static final value representing the subclass with a similar name. The full is RelabelTheSmallerHalf
+	 * Instead of using isInstanceof(), we just give this integer value as parameter.
+	 */
 	public static final int RELABEL = 2;
+	/**
+	 * A static final value representing the subclass with the same name. 
+	 * Instead of using isInstanceof(), we just give this integer value as parameter.
+	 */
 	public static final int SIMPLETREE = 3;
+	/**
+	 * A static final value representing the subclass with the same name. 
+	 * Instead of using isInstanceof(), we just give this integer value as parameter.
+	 */
 	public static final int WEIGHTEDUNION = 4;
+	/**
+	 * A static final value representing the subclass with the same name. 
+	 * Instead of using isInstanceof(), we just give this integer value as parameter. 
+	 */
 	public static final int PATHCOMPRESSION = 5;
+	protected int NULL = -1;
 	
 	
 	/**
@@ -46,5 +67,11 @@ public abstract class UnionFindFrame {
 	 * @param newSetName The name of the new set that should be used.
 	 */
 	public abstract void union(int setNameOne, int setNameTwo, int newSetName);
+
+	/**
+	 * Help method for printing the local variables in the subclasses. 
+	 * @param msg some extra message to print before the local variables of the classes.
+	 */
+	public abstract void logNames(String msg);
 	
 }
